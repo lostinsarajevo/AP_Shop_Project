6df02578343b7ed235f4543faf37d3bd30c18811
qonu <tarik.hamzic1991@gmail.com>
2019-10-28 20:40:25 +0100

    Second commit, added the entry pages with routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntryPageComponent } from './entry-page/entry-page.component';
+import { LoginPageComponent } from './entry-page/login-page/login-page.component';
+import { RegisterPageComponent } from './entry-page/register-page/register-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Login', component: LoginPageComponent},
+  {path: 'Register', component: RegisterPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntryPageComponent } from './entry-page/entry-page.component';
+import { LoginPageComponent } from './entry-page/login-page/login-page.component';
+import { RegisterPageComponent } from './entry-page/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
