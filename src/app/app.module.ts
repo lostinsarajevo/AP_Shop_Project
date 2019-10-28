e7ace4a54f9cf653d1f45574836e43f6e02f0d3a
qonu <tarik.hamzic1991@gmail.com>
2019-10-28 21:47:31 +0100

    Third commit. Implemented the search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { EntryPageComponent } from './entry-page/entry-page.component';
 import { LoginPageComponent } from './entry-page/login-page/login-page.component';
 import { RegisterPageComponent } from './entry-page/register-page/register-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntryPageComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    HomePageComponent,
+    SearchComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
