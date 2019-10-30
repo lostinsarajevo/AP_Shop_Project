9f6e52703b6a71fea2bc1e5bec140b0b94574e48
qonu <tarik.hamzic1991@gmail.com>
2019-10-30 21:44:13 +0100

    modified the search bar and reworked the styling of the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { EntryPageComponent } from './entry-page/entry-page.component';
 import { LoginPageComponent } from './entry-page/login-page/login-page.component';
 import { RegisterPageComponent } from './entry-page/register-page/register-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
+  {path: '', component: EntryPageComponent},
   {path: 'Login', component: LoginPageComponent},
-  {path: 'Register', component: RegisterPageComponent}
+  {path: 'Register', component: RegisterPageComponent},
+  {path: 'Home', component: HomePageComponent}
 ];
 
 @NgModule({
